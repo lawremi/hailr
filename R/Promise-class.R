0b1c8b816580ca3135f36ea3aec1a9ecf3b00cb6
Michael Lawrence <michafla@gene.com>
2018-07-22 10:22:57 -0700

    add basic promise coercion logic from rsolr

--- R/Promise-class.R
+++ R/Promise-class.R
@@ -11,3 +11,35 @@ setClass("SimplePromise",
          slots=c(expr="Expression",
                  context="Context"),
          contains="Promise")
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Accessors
+###
+
+context <- function(x) x@context
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Fulfillment
+###
+
+setGeneric("fulfill", function(x, ...) standardGeneric("fulfill"))
+setMethod("fulfill", "Promise", function(x) {
+    eval(expr(x), context(x))
+})
+setMethod("fulfill", "ANY", function(x) x)
+
+setMethod("as.logical", "Promise", function(x) as.logical(fulfill(x)))
+setMethod("as.integer", "Promise", function(x) as.integer(fulfill(x)))
+setMethod("as.numeric", "Promise", function(x) as.numeric(fulfill(x)))
+setMethod("as.character", "Promise", function(x) as.character(fulfill(x)))
+setMethod("as.factor", "Promise", function(x) as.factor(fulfill(x)))
+setMethod("as.vector", "Promise",
+          function(x, mode = "any") as.vector(fulfill(x), mode=mode))
+as.Date.Promise <- function(x, ...) as.Date(fulfill(x))
+as.POSIXct.Promise <- function(x, ...) as.POSIXct(fulfill(x))
+as.POSIXlt.Promise <- function(x, ...) as.POSIXlt(fulfill(x))
+as.data.frame.Promise <- function(x, row.names = NULL, optional = FALSE, ...) {
+    as.data.frame(fulfill(x), row.names=row.names, optional=optional, ...)
+}
+
+as.list.Promise <- function(x, ...) as.list(fulfill(x))
